44a2b57cf1e2440292cba2e84bcaa77d7bbe3ebf
James Harrison <jameshfharrison@outlook.com>
2022-02-28 17:04:03 -0500

    working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { extendTheme } from "@chakra-ui/react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import Navbar from "../components/navbar";
 
 const colors = {
   brand: {
@@ -14,9 +15,12 @@ const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <ChakraProvider theme={theme}>
+        <Navbar />
+        <Component key={pageProps} {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
